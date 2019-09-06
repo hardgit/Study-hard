75624d3231390c858477a8327508028f6d255d54
hard_f <15711010690@163.com>
2019-09-06 18:29:04 +0800

    添加注释

--- app/src/main/java/com/android/myview/MyView.java
+++ app/src/main/java/com/android/myview/MyView.java
@@ -13,13 +13,13 @@ import android.view.View;
  * author : zf
  * date   : 2019/9/6
  * You are the best.
+ *  基础
  */
 public class MyView extends View {
 
     private Paint paint; //圆形
     private Paint paint2; //矩形
     private Paint paint3;
-    private int color = 0xaaff0000;
 
 
     public MyView(Context context) {
@@ -33,12 +33,12 @@ public class MyView extends View {
 
     public void initView(){
         paint = new Paint();
-        paint.setColor(color);
+        paint.setColor(0xaaff0000);
         paint.setAntiAlias(true);//抗锯齿
         paint.setStyle(Paint.Style.FILL);
 
         paint2 = new Paint();
-        paint2.setColor(0xfff000);
+        paint2.setColor(0xaafff000);
         paint2.setStyle(Paint.Style.STROKE);
         paint2.setAntiAlias(true);//抗锯齿
 
@@ -55,14 +55,15 @@ public class MyView extends View {
         canvas.drawCircle(300,110,100,paint);
 
         /**   RectF设置矩形的位置
-         *  left 子view左上角距离父控件左边的距离
-         *  top  子view左上角距离父控件左上方距离
-         *  right 子view右下角距父控件左边的距离  （决定画出的图形矩形宽度）
-         *  bottom 子view底部距父控件顶部的距离    （决定画出图形的高度）
+         *  left 子view左上角距离父view左边的距离
+         *  top  子view左上角距离父view左上方距离
+         *  right 子view右下角距父view左边的距离  （决定画出的矩形宽度  矩形的宽 = left（子View距父view左边的距离）+ 你想要的宽度）
+         *  bottom 子view底部距父view顶部的距离    （决定画出矩形的高度  同上   top+你想要的高度   ）
+         *
          */
-        RectF rectF = new RectF(100, 320, 120, 325);
-
-        canvas.drawRoundRect(rectF,10,10,paint);
+        RectF rectF = new RectF(100, 320, (100+400), (320+400) );
+        //绘制圆角矩形
+        canvas.drawRoundRect(rectF,10,10,paint2);
 
 
 
